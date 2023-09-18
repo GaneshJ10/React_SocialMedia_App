fa0d3fb443fd6c51a1adf4f1bde6e9cf56b8cf20
GaneshJ10 <ganeshjaishankar11@gmail.com>
2023-09-18 17:04:28 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        share your recent trips and events with us.
+      </p>
+    </main>
+  );
+};
+
+export default About;
---
+++ src/EditPost.js
@@ -0,0 +1,57 @@
+import { useContext, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const EditPost = () => {
+  const { posts, handleEdit, editBody, seteditBody, editTitle, seteditTitle } =
+    useContext;
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+
+  useEffect(() => {
+    if (post) {
+      seteditTitle(post.title);
+      seteditBody(post.body);
+    }
+  }, [post, seteditTitle, seteditBody]);
+
+  return (
+    <main className="NewPost">
+      {editTitle && (
+        <>
+          <h2>Edit Post</h2>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTitle">Title:</label>
+            <input
+              id="postTitle"
+              type="text"
+              required
+              value={editTitle}
+              onChange={(e) => seteditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody">Post:</label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => seteditBody(e.target.value)}
+            />
+            <button type="submit" onClick={() => handleEdit(post.id)}>
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+      {!editTitle && (
+        <>
+          <h2>Post Not Found</h2>
+          <p>Well, that's disappointing.</p>
+          <p>
+            <Link to="/">Visit Our Homepage</Link>
+          </p>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default EditPost;
